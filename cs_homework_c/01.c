af9fe03b73b539951439824f03896f479f02682c
wuuntt <wuuntt21@gmail.com>
2025-03-19 21:39:39 +0800

    update

--- cs_homework_c/01.c
+++ cs_homework_c/01.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
+int main() {
     int a, b, c;
     double x1, x2, D;
 
     scanf("%d%d%d", &a, &b, &c);
     D = b*b-4*a*c;
     
-    if (D >= 0)
-    {
+    if (D >= 0) {
         x1 = ((-b)+sqrt(b*b-4*a*c))/(2*a);
         x2 = ((-b)-sqrt(b*b-4*a*c))/(2*a);
-        if (x1 == x2)
-        {
+        if (x1 == x2) {
             printf("%.1f", x1);
         }
-        else
-        {
+        else {
             printf("%.1f\n%.1f", x1, x2);
         }
     }
 
-    else
-    {
+    else {
         x1 = ((-1.0)*b)/(2*a);
         x2 = sqrt((-1.0)*D)/(2*a);
         if (x1 == -0.0) x1 = 0.0;
